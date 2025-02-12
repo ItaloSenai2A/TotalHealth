9d478847cb42889b447fb3c78a9b0340016ea40e
Rayssa <rayssa.senaiii@gmail.com>
2025-02-12 14:54:31 -0300

    Adicionando Medico Controller

---
+++ Controllers/MedicosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TotalHealth.Data;
+using TotalHealth.Models;
+
+namespace TotalHealth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicosController : ControllerBase
+    {
+        private readonly TotalHealthDBContext _context;
+
+        public MedicosController(TotalHealthDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Medicos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medico>>> GetMedicos()
+        {
+            return await _context.Medicos.ToListAsync();
+        }
+
+        // GET: api/Medicos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medico>> GetMedico(Guid id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return medico;
+        }
+
+        // PUT: api/Medicos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedico(Guid id, Medico medico)
+        {
+            if (id != medico.MedicoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medico).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Medicos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Medico>> PostMedico(Medico medico)
+        {
+            _context.Medicos.Add(medico);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMedico", new { id = medico.MedicoId }, medico);
+        }
+
+        // DELETE: api/Medicos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMedico(Guid id)
+        {
+            var medico = await _context.Medicos.FindAsync(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            _context.Medicos.Remove(medico);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MedicoExists(Guid id)
+        {
+            return _context.Medicos.Any(e => e.MedicoId == id);
+        }
+    }
+}
