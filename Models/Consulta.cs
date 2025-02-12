2646f89502d0f2a2e021742ef13391584d80c5b5
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 14:03:56 -0300

    inserido atributos na class consulta

--- Models/Consulta.cs
+++ Models/Consulta.cs
@@ -2,5 +2,17 @@
 {
     public class Consulta
     {
+        public Guid ConsultaId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public Guid MedicoId { get; set; }
+        public Medico Medico { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Status { get; set; }
+        public Guid EspecialidadeId { get; set; }
+        public Especialidade Especialidade { get; set; }
+        public decimal Valor { get; set; }
+        public ICollection<Prescricao> Prescricoes { get; set; }
+        public ICollection<Pagamento> Pagamentos { get; set; }
     }
 }
