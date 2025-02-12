450912764e88cc17916455eeb51faafc88826080
Rayssa <rayssa.senaiii@gmail.com>
2025-02-12 14:18:22 -0300

    Adicionando a class MedicoEspecialidade e seus respectivos atributos

---
+++ Models/MedicoEspecialidade.cs
@@ -0,0 +1,10 @@
+﻿using TotalHealth.Models;
+
+public class MedicoEspecialidade
+{
+    public Guid MedicoEspecialidadeId { get; set; }
+    public Guid MedicoId { get; set; }
+    public Medico Medico { get; set; }
+    public Guid EspecialidadeId { get; set; }
+    public Especialidade Especialidade { get; set; }
+}
