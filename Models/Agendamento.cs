a124c735904540079182dbdd7deecb4d174c20d4
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 14:01:49 -0300

    inserido atributos na class agendamento

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -2,6 +2,14 @@
 {
     public class Agendamento
     {
+        public Guid AgendamentoId { get; set; }
+        public Guid ConsultaId { get; set; }
+        public Consulta Consulta { get; set; }
+        public DateTime DataHora { get; set; }
+        public Guid MedicoId { get; set; }
+        public Medico Medico { get; set; }
+        public Guid EspecialidadeId { get; set; }
+        public Especialidade Especialidade { get; set; }
 
     }
 }
