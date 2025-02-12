c6bde5b76a35b7757fa3f963299637eee712651a
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 14:05:16 -0300

    inserido atributos na class contato

--- Models/Contato.cs
+++ Models/Contato.cs
@@ -2,5 +2,10 @@
 {
     public class Contato
     {
+        public Guid ContatoId { get; set; }
+        public string Tipo { get; set; }
+        public string Valor { get; set; }
+        public string Endereco { get; set; }
+        public string RedesSociais { get; set; }
     }
 }
