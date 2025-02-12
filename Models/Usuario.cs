8b4ab8b7a93b623ee46f600a5a7fce1e3f576195
Rayssa <rayssa.senaiii@gmail.com>
2025-02-12 14:05:40 -0300

    Incluindo atributos na class Usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,12 +1,18 @@
-﻿namespace TotalHealth.Models
-{
-    public class Usuario
-    {
-        public Guid UsuarioId { get; set; }
-        public string? Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string? Telefone { get; set; }
+﻿using System;
+using System.Collections.Generic;
+using TotalHealth.Models;
 
-    }
+public class Usuario
+{
+    public Guid UsuarioId { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+    public string Telefone { get; set; }
+    public string Cpf { get; set; }
+    public string Genero { get; set; }
+    public string TipoSanguineo { get; set; }
+    public string Endereco { get; set; }
+    public ICollection<Exame> Exames { get; set; }
+    public ICollection<Consulta> Consultas { get; set; }
 }
