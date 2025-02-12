6b8fd7fafa3bb071e37b2b410108c3f6deda4364
Ítalo <italooo.senai@gmail.com>
2025-02-12 10:58:50 -0300

    Solução de problemas

---
+++ Data/TotalHealthDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TotalHealth.Models;
+
+namespace TotalHealth.Data
+{
+    public class TotalHealthDBContext : IdentityDbContext
+    {
+        public TotalHealthDBContext(DbContextOptions<TotalHealthDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace TotalHealth.Models
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public string? Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string? Telefone { get; set; }
+
+    }
+}
