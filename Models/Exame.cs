09ee0c7accc07da523ee09f7fba6dba0904a53cf
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 14:08:33 -0300

    inserido atributos na class exame

--- Models/Exame.cs
+++ Models/Exame.cs
@@ -2,5 +2,14 @@
 {
     public class Exame
     {
+        public Guid ExameId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public Guid PrescricaoId { get; set; }
+        public Prescricao Prescricao { get; set; }
+        public decimal Valor { get; set; }
+        public ICollection<Pagamento> Pagamentos { get; set; }
     }
 }
