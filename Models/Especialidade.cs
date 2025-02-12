40b7a89f7d33a32677eb488a5f2c0861466f73a9
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 14:07:04 -0300

    incluido atributos na class especialidade

--- Models/Especialidade.cs
+++ Models/Especialidade.cs
@@ -2,5 +2,10 @@
 {
     public class Especialidade
     {
+        public Guid EspecialidadeId { get; set; }
+        public string Nome { get; set; }
+        public ICollection<MedicoEspecialidade> MedicoEspecialidades { get; set; }
+        public ICollection<Consulta> Consultas { get; set; }
+        public ICollection<Agendamento> Agendamentos { get; set; }
     }
 }
