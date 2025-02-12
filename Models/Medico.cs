8b2caf5d9444d2276e195c9e1bc4f1195fd0712b
Emanuelly <emanuellydssenai@gmail.com>
2025-02-12 13:58:56 -0300

    inserido atributos na class medico

--- Models/Medico.cs
+++ Models/Medico.cs
@@ -2,5 +2,11 @@
 {
     public class Medico
     {
+        public Guid MedicoId { get; set; }
+        public string Nome { get; set; }
+        public string Crm { get; set; }
+        public ICollection<MedicoEspecialidade> MedicoEspecialidades { get; set; }
+        public ICollection<Consulta> Consultas { get; set; }
+        public ICollection<Agendamento> Agendamentos { get; set; }
     }
 }
