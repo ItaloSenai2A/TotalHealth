bd469275db84ecc19a845456a9156d3f9f3d673b
Rayssa <rayssa.senaiii@gmail.com>
2025-02-12 14:08:05 -0300

    Incluindo atributos na class Prescricao

--- Models/Prescricao.cs
+++ Models/Prescricao.cs
@@ -1,6 +1,10 @@
-﻿namespace TotalHealth.Models
+﻿using TotalHealth.Models;
+
+public class Prescricao
 {
-    public class Prescricao
-    {
-    }
+    public Guid PrescricaoId { get; set; }
+    public Guid ConsultaId { get; set; }
+    public Consulta Consulta { get; set; }
+    public string Descricao { get; set; }
+    public ICollection<Exame> Exames { get; set; }
 }
