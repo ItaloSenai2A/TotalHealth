f2d4c1996ba93b3bafdb721346433b3f36cced07
Ítalo <italooo.senai@gmail.com>
2025-02-18 08:09:45 -0300

    Adicionando Endpoints no AgendamentosController

--- Models/Agendamento.cs
+++ Models/Agendamento.cs
@@ -5,11 +5,11 @@
         public Guid AgendamentoId { get; set; }
         public Guid ConsultaId { get; set; }
         public Consulta Consulta { get; set; }
-        public DateTime DataHora { get; set; }
+        public DateTime Data { get; set; }
         public Guid MedicoId { get; set; }
         public Medico Medico { get; set; }
         public Guid EspecialidadeId { get; set; }
         public Especialidade Especialidade { get; set; }
-
+        public string Status { get; internal set; }
     }
 }
